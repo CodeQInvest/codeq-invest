458243aa6684861c71b376d7f2b784794759299e
Felix Müller <felix.mueller.berlin@googlemail.com>
2013-05-16 21:23:34 +0200

    added sonar connectivity checker service

---
+++ metrics-collector/src/main/java/org/codeqinvest/sonar/SonarConnectionCheckerService.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2013 Felix Müller
+ *
+ * This file is part of CodeQ Invest.
+ *
+ * CodeQ Invest is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CodeQ Invest is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CodeQ Invest.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.codeqinvest.sonar;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.methods.HttpGet;
+import org.sonar.wsclient.Host;
+import org.sonar.wsclient.connectors.HttpClient4Connector;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+/**
+ * TODO javadoc
+ *
+ * @author fmueller
+ */
+@Slf4j
+@Service
+public class SonarConnectionCheckerService {
+
+  private static final int OK = 200;
+
+  public boolean isReachable(String host) {
+    return isReachable(host, null, null);
+  }
+
+  public boolean isReachable(String host, String username, String password) {
+    HttpClient4Connector connector = new HttpClient4Connector(new Host(host, username, password));
+    try {
+      return connector.getHttpClient().execute(new HttpGet(host + "/api/metrics")).getStatusLine().getStatusCode() == OK;
+    } catch (IOException e) {
+      SonarConnectionCheckerService.log.info("Sonar is not reachable during connection check.", e);
+      return false;
+    }
+  }
+}
---
+++ metrics-collector/src/test/java/org/codeqinvest/sonar/SonarConnectionCheckerServiceIntegrationTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 Felix Müller
+ *
+ * This file is part of CodeQ Invest.
+ *
+ * CodeQ Invest is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CodeQ Invest is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CodeQ Invest.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.codeqinvest.sonar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:META-INF/spring/module-context.xml")
+public class SonarConnectionCheckerServiceIntegrationTest {
+
+  @Autowired
+  private SonarConnectionCheckerService connectionCheckerService;
+
+  @Test
+  public void nemoSonarDemoServerShouldBeReachable() {
+    assertThat(connectionCheckerService.isReachable("http://nemo.sonarsource.org")).isTrue();
+  }
+
+  @Test
+  public void whenServerIsNotSonarItShouldBeUnreachable() {
+    assertThat(connectionCheckerService.isReachable("http://google.com")).isFalse();
+  }
+}
